80fdb6c3775e6cc36817578f75e40e39570bc46c
Bogdan Alov <alovbg21@abv.bg>
2017-03-27 13:00:53 +0300

    more excercises

---
+++ Databases Advanced - Entity Framework/8. Best Practices and Architecture/Best Practices and Architecture/Best Practices and Architecture/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_Practices_and_Architecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/8. Best Practices and Architecture/Best Practices and Architecture/Employees/Client/Startup.cs
@@ -0,0 +1,165 @@
+﻿using Employees.Data;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Employees
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            EmployeesContext context = new EmployeesContext();
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            long timePassed = 0L;
+
+            int testCount = 10; // Amount of tests to perform
+
+            for (int i = 0; i < testCount; i++)
+
+            {
+
+                // Clear all query cache
+
+                context.Database.ExecuteSqlCommand("CHECKPOINT; DBCC DROPCLEANBUFFERS; ");
+
+                stopwatch.Start();
+
+                //QueryWithEagerLoading(context);
+                //QueryWithLazyLoading(context);
+
+                //QueryWithEagerLoadingSelect(context);
+                //QueryWithLazyLoadingSelect(context);
+
+                //SalaryBelow3000Lazy(context);
+                //SalaryBelow3000Eager(context);
+
+                //EmployeesWithOneProjectLazy(context);
+                //EmployeesWithOneProjectEager(context);
+
+                //OrderBy1(context);
+                //OrderBy2(context);
+
+                OptimizeQuery(context);
+
+                stopwatch.Stop();
+
+                timePassed += stopwatch.ElapsedMilliseconds;
+
+                stopwatch.Reset();
+
+            }
+
+            TimeSpan averageTimePassed = TimeSpan.FromMilliseconds(timePassed / (double)testCount);
+
+            Console.WriteLine(averageTimePassed);
+        }
+
+        private static void OptimizeQuery(EmployeesContext context)
+        {
+            var employees = context.Employees.Where(c => c.Subordinates.Any(s => s.Address.Town.Name.StartsWith("B"))).Select(c => c.FirstName).Distinct().ToList();
+            foreach (var emp in employees)
+            {
+                string result = $"{emp}";
+            }
+        }
+
+        private static void OrderBy2(EmployeesContext context)
+        {
+            var employees = context.Employees.ToList().OrderBy(d => d.Department.Name).ThenBy(d => d.FirstName);
+            foreach (var emp in employees)
+            {
+                var result = $"{emp.FirstName} - {emp.Department.Name}";
+            }
+        }
+
+        private static void OrderBy1(EmployeesContext context)
+        {
+            var employees = context.Employees.OrderBy(d => d.Department.Name).ThenBy(d => d.FirstName).ToList();
+            foreach (var emp in employees)
+            {
+                var result = $"{emp.FirstName} - {emp.Department.Name}";
+            }
+        }
+
+        private static void EmployeesWithOneProjectEager(EmployeesContext context)
+        {
+            var employees = context.Employees.Include("Project").Include("Department").Include("EmployeesProject").Where(c => c.EmployeesProjects.Count == 1).Select(c => new { Name = c.FirstName + " " + c.LastName, DepartmentName = c.Department.Name }).ToList();
+            foreach (var e in employees)
+            {
+                var result = $"{e.Name} - {e.DepartmentName}";
+            }
+        }
+
+        private static void EmployeesWithOneProjectLazy(EmployeesContext context)
+        {
+            var employees = context.Employees.Where(c => c.EmployeesProjects.Count == 1).Select(c => new { Name = c.FirstName + " " + c.LastName, DepartmentName = c.Department.Name }).ToList();
+            foreach (var e in employees)
+            {
+                var result = $"{e.Name} - {e.DepartmentName}";
+            }
+        }
+
+        private static void SalaryBelow3000Eager(EmployeesContext context)
+        {
+            var employees = context.Employees.Include("Department").Include("Address").Where(c => c.Salary < 3000).Select(c => new { FirstName = c.FirstName, DepartmentName = c.Department.Name, Adress = c.Address.AddressText }).ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.DepartmentName} - {emp.Adress}";
+            }
+        }
+
+        private static void SalaryBelow3000Lazy(EmployeesContext context)
+        {
+            var employees = context.Employees.Where(c => c.Salary < 3000).Select(c => new { FirstName = c.FirstName, DepartmentName = c.Department.Name, Adress = c.Address.AddressText }).ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.DepartmentName} - {emp.Adress}";
+            }
+        }
+
+        private static void QueryWithLazyLoadingSelect(EmployeesContext context)
+        {
+            var employees = context.Employees.Select(c => new { FirstName = c.FirstName, DepartmentName = c.Department.Name, Adress = c.Address.AddressText }).ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.DepartmentName} - {emp.Adress}";
+            }
+        }
+
+        private static void QueryWithEagerLoadingSelect(EmployeesContext context)
+        {
+            var employees = context.Employees.Include("Department").Include("Address").Select(c => new { FirstName = c.FirstName, DepartmentName = c.Department.Name, Adress = c.Address.AddressText }).ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.DepartmentName} - {emp.Adress}";
+            }
+        }
+
+        private static void QueryWithEagerLoading(EmployeesContext context)
+        {
+            var employees = context.Employees.Include("Department").Include("Address").ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.Department.Name} - {emp.Address.AddressText}";
+            }
+        }
+
+        private static void QueryWithLazyLoading(EmployeesContext context)
+        {
+            var employees = context.Employees.ToList();
+
+            foreach (var emp in employees)
+            {
+                string result = $"{emp.FirstName} - {emp.Department.Name} - {emp.Address.AddressText}";
+            }
+        }
+    }
+}
